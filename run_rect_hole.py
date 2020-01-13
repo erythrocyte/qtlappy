63e60c66b6a15ca8c0e9c43b11c04dd201fafb09
erythrocyte_at <marseille.tatar@yahoo.com>
2020-01-13 10:17:22 +0300

    "bound area with simple triangulation without any tight mesh completed"

--- run_rect_hole.py
+++ run_rect_hole.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from laplapy.readers import bound_reader, holes_reader
+from laplapy.readers import bound_reader  # ,  holes_reader
 from laplapy.classes import gridTri2d
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,19 +17,22 @@ def main():
     bound_points = bound_reader.read_bound_file(fn_bound_points)
 
     # read holes
-    fn_holes = 'examples/data/rect_hole/holes.txt'
-    fn_holes = os.path.join(dirname, fn_holes)
-    holes_points = holes_reader.read_holes_file(fn_holes)
+    # fn_holes = 'examples/data/rect_hole/holes.txt'
+    # fn_holes = os.path.join(dirname, fn_holes)
+    # holes_points = holes_reader.read_holes_file(fn_holes)
 
     gridMaker = gridTri2d.GridTri2D()
     pts_bound, seg_bound = gridMaker._GridTri2D__polygon(bound_points)
 
     pts = np.vstack([pts_bound])
     seg = np.vstack([seg_bound])
-    A = dict(vertices=pts, segments=seg, holes=[[]])
+    A = dict(vertices=pts, segments=seg)  # , holes=[[]])
     B = tr.triangulate(A)
-    # tr.compare(plt, A, B)
-    # plt.show()
+    ax = plt.axes()
+    tr.plot(ax, **B)
+    ax.get_xaxis().set_visible(True)
+    ax.get_yaxis().set_visible(True)
+    plt.show()
 
 
 if __name__ == '__main__':
