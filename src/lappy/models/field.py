9b938962b31df0be81778bbaddc2baa81057a615
marseille <hamidullinmr@actual-technology.ru>
2020-12-17 12:15:11 +0300

    common well mesh settings class created, docstring for classes added.

--- src/lappy/models/field.py
+++ src/lappy/models/field.py
@@ -7,6 +7,14 @@ from src.lappy.models.bound import Bound
 from src.lappy.models.well import Well
 
 class Field(object):
+    """
+    Field class. Contains information about input geometry: bound and wells.
+
+    Args:
+        name (str): field name.
+        bound (Bound): domain bound.
+        wells (List[Well]): wells.
+    """
     def __init__(self, name, bound: Bound, wells: List[Well]):
         self.name = name
         self.bound = bound
@@ -14,9 +22,17 @@ class Field(object):
 
     @classmethod
     def create(cls, name: str, bound_file: str, wells_file: str):
+        """
+        Creates class instance from input json file for bound and wells.
+
+        Args:
+            name (str): field name.
+            bound_file (str): json file name which contains domain bound data.
+            wells_file (str): json file name which contains wells data.
+        """
         bound = Bound.from_json(bound_file)
         with open(wells_file, "r", encoding="utf-8") as f:
             fdata = f.read()
             data = json.loads(fdata)
-            wells = list(map(Well.from_json, data["wells"]))
+            wells = list(map(Well.from_dict, data["wells"]))
         return cls(name, bound=bound, wells=wells)
--- src/lappy/models/well.py
+++ src/lappy/models/well.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from typing import List
 from src.lappy.models.point import Point
 
 
 class Well(object):
-    def __init__(self, id: int, name: str, is_vert: bool, radius: float, track: list):
+    """
+    Well class.
+
+    Args:
+        id (int):               identification index;
+        name (str):             well name.
+        is_vert(bool):          vertical well attribute.
+        radius (float):         well radius.
+        track (List[Point]):    well trajectory.
+    """
+    def __init__(self, id: int, name: str, is_vert: bool, radius: float, track: List[Point]):
         self.id = id
         self.name = name
         self.is_vert = is_vert
@@ -13,7 +24,13 @@ class Well(object):
         self.track = track
 
     @ classmethod
-    def from_json(cls, data):
-        points = list(map(Point.from_json, data["points"]))
+    def from_dict(cls, data: dict):
+        """
+        Creates class instance from dictionary.
+
+        Args:
+            data (dict):    input data dictionary.
+        """
+        points = list(map(Point.from_dict, data["points"]))
         return cls(id=data['id'], name=data['name'], is_vert=data['vert'],
                    radius=data['radius'], track=points)
