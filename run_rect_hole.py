4b5bb63cde05446567b87ce39b658acd07914622
erythrocyte <marseille.tatar@yahoo.com>
2020-12-20 04:04:20 +0300

    error on triangulation

--- run_rect_hole.py
+++ run_rect_hole.py
@@ -3,22 +3,24 @@
 
 import os
 from src.lappy.models.field import Field
-# from src.lappy.models import bound
-# from laplapy.readers import bound_reader, holes_reader
-# from laplapy.classes import gridTri2d
-# # import numpy as np
-# import matplotlib.pyplot as plt
-# import triangle as tr
+from src.lappy.services.mesh_maker_2d import MeshMaker2D
+from src.lappy.models.settings.global_setts import GlobalSetts
+import matplotlib.pyplot as plt
+import triangle as tr
 
 
 def main():
-    # read boundary points
     dirname = os.path.join(os.path.realpath(''), 'examples/data/rect_hole')
     fn_bound = os.path.join(dirname, 'boundary.json')
     fn_wells = os.path.join(dirname, 'wells.json')
 
     field = Field.create("test_field", fn_bound, fn_wells)
-    print ("field created")
+    print("field created")
+
+    setts = GlobalSetts()
+    mesh_maker = MeshMaker2D()
+    mesh = mesh_maker.triangulate(field, setts)
+
     # bound_points = bound_reader.read_bound_file_json(fn_bound_points)
 
     # read holes
@@ -28,12 +30,12 @@ def main():
     # gridMaker = gridTri2d.GridTri2D()
     # A = gridMaker.make_tri_grid(bound_points, wells)
     # B = tr.triangulate(A, 'qpa0.1')
-    # ax = plt.axes()
-    # # tr.compare(plt, A, B)
-    # tr.plot(ax, **B)
-    # ax.get_xaxis().set_visible(True)
-    # ax.get_yaxis().set_visible(True)
-    # plt.show()
+    ax = plt.axes()
+    # tr.compare(plt, A, B)
+    tr.plot(ax, **mesh)
+    ax.get_xaxis().set_visible(True)
+    ax.get_yaxis().set_visible(True)
+    plt.show()
 
 
 if __name__ == '__main__':
