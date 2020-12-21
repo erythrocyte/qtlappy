1c41671df6e99b647487fa7dd08d2b47a1e61728
erythrocyte <marseille.tatar@yahoo.com>
2020-12-22 01:44:43 +0300

    unused code removed

--- run_rect_hole.py
+++ run_rect_hole.py
@@ -36,55 +36,19 @@ def a():
     import matplotlib.pyplot as plt
 
     fig, ax = plt.subplots()
-
-    N = 3
-    # x = np.random.rand(N)
-    # y = np.random.rand(N)
-    # radii = 0.1*np.random.rand(N)
     patches = []
-    # for x1, y1, r in zip(x, y, radii):
-    #     circle = Circle((x1, y1), r)
-    #     patches.append(circle)
-
-    # x = np.random.rand(N)
-    # y = np.random.rand(N)
-    # radii = 0.1*np.random.rand(N)
-    # theta1 = 360.0*np.random.rand(N)
-    # theta2 = 360.0*np.random.rand(N)
-    # for x1, y1, r, t1, t2 in zip(x, y, radii, theta1, theta2):
-    #     wedge = Wedge((x1, y1), r, t1, t2)
-    #     patches.append(wedge)
-
-    # Some limiting conditions on Wedge
-    # patches += [
-    #     Wedge((.3, .7), .1, 0, 360),             # Full circle
-    #     Wedge((.7, .8), .2, 0, 360, width=0.05),  # Full ring
-    #     Wedge((.8, .3), .2, 0, 45),              # Full sector
-    #     Wedge((.8, .3), .2, 45, 90, width=0.10),  # Ring sector
-    # ]
 
     xy = np.empty((0, 2))
     xy = np.append(xy, np.array([[-1, 1]]), axis=0)
     xy = np.append(xy, np.array([[2, 3]]), axis=0)
     xy = np.append(xy, np.array([[0, 0]]), axis=0)
     xy2 = np.array([(1, 1), (2, 3), (0, 0)])
-    # xy = np.append(xy, [2, 3])
-    # xy = np.append(xy, [0, 0])
-    # for i in range(10):
-    #     np.append(xy, (i, i * i - i))
 
-    # xy = np.array([(1, 1)])
     pol = Polygon(xy, True)
     patches.append(pol)
 
-    
     pol = Polygon(xy2, True)
     patches.append(pol)
-
-    # for i in range(N):
-    #     xy = np.random.rand(N, 2)
-    #     polygon = Polygon(xy, True)
-    #     patches.append(polygon)
 
     colors = 12*np.random.rand(len(patches))
     p = PatchCollection(patches, alpha=0.4)
